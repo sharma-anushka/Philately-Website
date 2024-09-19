6d6142d37358f82fd9e72c1a0ad94c6300ab1d01
Aanvi <144897798+Aanvig2@users.noreply.github.com>
2024-09-19 10:20:38 +0530

    Create profileData.js

---
+++ profileData/profileData.js
@@ -0,0 +1,97 @@
+const philatelistProfiles = {
+    name: "Karuna Chhatri",
+    profilePicture: {
+        filename: "profile1",
+        url: "https://i.ibb.co/5sJ8GML/image.png",
+    },
+    location: "Mumbai, Maharashtra, India",
+    bio: "A lifelong enthusiast of Indian stamps, fascinated by their historical and cultural significance. Stamps are windows into the rich tapestry of India's history, culture, and art.",
+    badges: [
+        {
+            name : "Expert Stamp Identifier",
+            image : "https://i.ibb.co/sCBMFmS/image.png"
+        },
+        {
+            name : "Philatelic Historian",
+            image : "https://i.ibb.co/pxVkk6C/image.png"
+        },
+        {
+            name : "Master Collector",
+            image : "https://i.ibb.co/6PH8z4W/image.png"
+        }
+    ],
+    collections: {
+        "Freedom Struggle": [
+            {
+                name: "Mahatma Gandhi 1948: The first commemorative stamp issued after independence",
+                image: "https://upload.wikimedia.org/wikipedia/commons/1/16/Official_stamp_of_India_Mahatma_Gandhi_1948_10_Rupees.jpg"
+            },
+            {
+                name: "Salt March 1980: Marking the 50th anniversary of Gandhi's Salt March",
+                image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Salt_Satyagraha_%28stamp_issued_in_1980%29.jpg"
+            },
+            {
+                name: "Quit India Movement 1992: Commemorating the movement that galvanized India's freedom fight",
+                image: "https://upload.wikimedia.org/wikipedia/commons/2/22/Quit_India_Movement_1992_stamp_2.jpg"
+            }
+        ],
+        "Indian Art and Architecture": [
+            {
+                name: "Ajanta Caves 1955: A tribute to the stunning ancient rock-cut caves",
+                image: "https://www.philindiastamps.com/cdn/shop/products/MYS-31_2_1024x1024.jpg?v=1634466248"
+            },
+            {
+                name: "Brihadeeswarar Temple 1987: Stamps celebrating the architectural marvel of Tamil Nadu",
+                image: "https://www.philindiastamps.com/cdn/shop/products/MYS-91_1024x1024.jpg?v=1534222979"
+            },
+            {
+                name: "Konark Sun Temple 1985: Depicting the intricate carvings of the Odisha monument",
+                image: "https://www.philindiastamps.com/cdn/shop/products/1_4e33e773-36ec-4a22-942d-a0245634590b_1024x1024.jpg?v=1535037262"
+            }
+        ],
+        "Indian Railways": [
+            {
+                name: "Locomotives of India 1976: Commemorating the history of Indian Railways",
+                image: "https://www.philindiastamps.com/cdn/shop/files/img053_1024x1024.jpg?v=1717062173"
+            },
+            {
+                name: "Vintage Rail Coach 2015: Celebrating India's railway heritage",
+                image: "https://www.stampexindia.com/image/product/2016/08/07/14705864281.jpg"
+            },
+            {
+                name: "Kalka-Shimla Railway 2008: Marking the UNESCO World Heritage site",
+                image: "https://www.philindiastamps.com/cdn/shop/products/p2008_1024x1024.jpg?v=1655470309"
+            }
+        ],
+        "Cultural Icons": [
+            {
+                name: "Rabindranath Tagore Painting 1987: A tribute to the poet and philosopher",
+                image: "https://www.philindiastamps.com/cdn/shop/products/p1077_1024x1024.jpg?v=1651401600"
+            },
+            {
+                name: "Swami Vivekananda 1963: Honoring the spiritual leader",
+                image: "https://www.philindiastamps.com/cdn/shop/products/p380_1024x1024.jpg?v=1543226526"
+            },
+            {
+                name: "Satyajit Ray 1994: A tribute to the legendary filmmaker",
+                image: "https://www.philindiastamps.com/cdn/shop/products/p1403_1024x1024.jpg?v=1670784108"
+            }
+        ]
+    },
+    blogPosts: [
+        {
+            title: "Gandhi’s Legacy: The 1948 Commemorative Stamp",
+            excerpt: "The first stamp of independent India, commemorating Mahatma Gandhi’s life and leadership."
+        },
+        {
+            title: "Rabindranath Tagore 1961: A Tribute to a Cultural Icon",
+            excerpt: "How the 1961 Rabindranath Tagore stamp commemorated the poet’s legacy."
+        },
+        {
+            title: "Indira Gandhi 1985: Honoring India’s First Female Prime Minister",
+            excerpt: "A look at how the 1985 Indira Gandhi commemorative stamp honored her life and achievements."
+        }
+    ]
+};
+
+module.exports = philatelistProfiles;
